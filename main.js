ceb5879e85e86b72d2b5991f81f28e778bbc6b0b
akchichemohamedaymen <akchiche.mohamedaymen@gmail.com>
2024-06-20 16:34:00 +0200

    I finished the women task

--- main.js
+++ main.js
@@ -1,48 +1,57 @@
 let featureds = document.getElementById("featureds");
-let featured = (src , name = "NIKE JORDAN")=>{
-    let div = document.createElement("div");
+let women = document.querySelector(".women");
+let createLink = ( val , href = "#" )=>{
     let a = document.createElement("a");
-    let a1 = document.createElement("a");
+    a.href =  href;
+    a.innerHTML = val;
+    a.style.display = "flex";
+    a.style.alignItems = "center";
+    a.style.gap = "10px";
+    return a;
+}
+let createImage = (src)=>{
     let image = document.createElement("img");
-    let h3 = document.createElement("h3");
-    let h_3 = document.createElement("h3");
-    div.className = "featured";
-    a.href = a1.href = "#";
-    a.innerHTML = "Sale"
-    a1.innerHTML = `Add to card  <i class="fa-solid fa-arrow-right"></i>`
     image.src = src;
-    h3.innerHTML = name;
-    h_3.innerHTML = "149.99$";
-    div.appendChild(a);
-    div.appendChild(image);
-    div.appendChild(h3);
-    div.appendChild(h_3);
-    div.appendChild(a1);
+    return image;
+}
+createH3 =(val)=>{
+    let h3 = document.createElement("h3");
+    h3.innerHTML = val;
+    return h3;
+}
+let createDiv = (src ,  cl = '' , name = "NIKE JORDAN")=>{
+    let div = document.createElement("div");
+    div.className = cl;
+    div.appendChild(createLink("Sale"));
+    div.appendChild(createImage(src));
+    div.appendChild(createH3(name));
+    div.appendChild(createH3("149.99$"));
+    div.appendChild(createLink(`Add to card <i class="fa-solid fa-arrow-right"></i>`));
     return div;
 }
 
-let featurds = (src , name = "NIKE JORDAN")=>{
+let createDiv2 = (src , cl = '' ,  name = "NIKE JORDAN")=>{
     let div = document.createElement("div");
-    let a1 = document.createElement("a");
-    let image = document.createElement("img");
-    let h3 = document.createElement("h3");
     let p = document.createElement("p");
-    div.className = "featured";
-    image.src = src;
-    h3.innerHTML = name;
-    p.innerHTML = "New Collection in 2024"
-    a1.href = "#";
-    a1.innerHTML = `Add to card  <i class="fa-solid fa-arrow-right"></i>`;
     let div2 = document.createElement("div");
-    div2.appendChild(h3);
+    div.className = cl ;
+    p.innerHTML = "New Collection in 2024"
+    div2.appendChild(createH3(name));
     div2.appendChild(p);
-    div2.appendChild(a1);
+    div2.appendChild(createLink(`Add to card  <i class="fa-solid fa-arrow-right"></i>`));
     div.appendChild(div2);
-    div.appendChild(image);
+    div.appendChild(createImage(src));
     return div;
 
 }
 for( i = 1 ; i < 4 ; i++)
-    featureds.appendChild(featured(`IMAGES/featured${i}.png`));
+    featureds.appendChild(createDiv(`IMAGES/featured${i}.png` ,  "featured"));
 for( i = 1 ; i <3 ; i++)
-    featureds.appendChild(featurds(`IMAGES/collection${i}.png`));
+    featureds.appendChild(createDiv2(`IMAGES/collection${i}.png` , "featured"));
+let arr = ["Nike Free TR" , "Nike Free TR" , "Nike GS Pink" , "Nike GET 5" ]
+for( i = 1 ; i <= 4 ; i++){
+    let el = createDiv(`IMAGES/women${i}.png` , "woman");
+    el.querySelector("h3").innerHTML = arr[i-1];
+    el.removeChild(el.querySelector("a"));
+    women.appendChild(el)
+}
